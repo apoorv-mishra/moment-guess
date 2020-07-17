a6e899dc8cf85e08a97ed38d602bbebdd0125ab9
Apoorv Mishra <apoorvmishra101092@gmail.com>
2020-07-18 00:52:56 +0530

    Add CLI support
    Add cli.js

---
+++ cli.js
@@ -0,0 +1,79 @@
+import chalk from 'chalk';
+import arg from 'arg';
+
+import guessFormat from './index.js';
+import pkg from './package';
+
+const warning = (message) => chalk`{yellow WARNING:} ${message}`;
+const info = (message) => chalk`{magenta INFO:} ${message}`;
+const error = (message) => chalk`{red ERROR:} ${message}`;
+
+const showHelp = () => console.log(
+	chalk`
+	{bold.cyan moment-guess} - {blue Utility for guessing date's format}
+
+	{bold USAGE}
+
+	{bold $} {cyan moment-guess} --date {yellow 2020-10-10}
+	{bold $} {cyan moment-guess} --version
+	{bold $} {cyan moment-guess} --help
+
+	{bold OPTIONS}
+
+	-h, --help                          Shows this help message
+
+	-v, --version                       Displays the current version of moment-guess
+
+	-d, --date                          Displays the provided date's format
+	`
+);
+const showUsage = () => console.log(
+	chalk`
+	{bold USAGE}
+
+	{bold $} {cyan moment-guess} --date {yellow 2020-10-10}
+	{bold $} {cyan moment-guess} --version
+	{bold $} {cyan moment-guess} --help
+	`
+);
+
+const showVersion = () => console.log(chalk.bold.white(pkg.version));
+
+(function() {
+	let args = null;
+	let config = {};
+	let date;
+
+	try {
+		args = arg({
+			'--help': Boolean,
+			'--version': Boolean,
+			'--date': String,
+			'--preference': String,
+			'-h': '--help',
+			'-v': '--version',
+			'-d': '--date',
+			'-p': '--preference',
+		});
+	} catch (err) {
+		console.error(error(err.message));
+		process.exit(1);
+	}
+
+	if (args['--help']) {
+		return showHelp()
+	}
+	if (args['--version']) {
+		return showVersion();
+	}
+	if (args['--date']) {
+		date = args['--date'];
+	}
+
+	if (!date) {
+		console.log(chalk`{bold.red Error:} {red Missing date!}`);
+		return showUsage();
+	}
+	console.log(chalk.bold.white(guessFormat(date, config)));
+})();
+
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import {terser} from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-export default {
+export default [{
 	input: 'index.js',
 	output: [
 		{
@@ -19,5 +21,22 @@ export default {
 			sourcemap: true,
 		},
 	],
-	plugins: [ resolve() ],
-}
+	plugins: [
+		resolve(),
+	],
+},{
+	input: 'cli.js',
+	output: [
+		{
+			file: pkg.bin['moment-guess'],
+			format: 'cjs',
+			name: 'MomentGuess',
+			banner: '#!/usr/bin/env node',
+		},
+	],
+	plugins: [
+		resolve(),
+		commonjs(),
+		json(),
+	],
+}];
