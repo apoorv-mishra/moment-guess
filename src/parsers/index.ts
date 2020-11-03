6690ada5e75c57d0ac40fa2259c0bbb2a1f7ddee
Apoorv Mishra <apoorvmishra101092@gmail.com>
2020-11-03 23:12:34 +0530

    🤷 Unneeded comment

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -218,10 +218,6 @@ const rFC2822DateTimeFormatParser = new Parser(
 		+ '(?<dayOfMonth>\\d{1,2})(?<delim3>\\s)(?<month>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)(?<delim4>\\s)(?<year>\\d{2,4})'
 		+ '(?<delim5>\\s)'
 		+ '(?<twentyFourHour>\\d{2})(?<delim6>:)(?<minute>\\d{2})(?:(?<delim7>:)(?<second>\\d{2}))?'
-		// Include space char as part of timezone group here
-		// to avoid timezone assigner
-		// to be confused with 'Z for timezone from ISO 8601'
-		// and 'Z for timezone from RFC 2822'
 		+ '(?<delim8>\\s)'
 		+ '(?<timezone>(?:(?:UT|GMT|[ECMP][SD]T)|[Zz]|[+-]\\d{4}))'
 		+ '$'
